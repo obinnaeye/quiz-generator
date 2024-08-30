9d6daa92d4f4dfd5d4d65e104c0de77fdc5bef41
glorycornel <chiomaowanku@gmail.com>
2024-08-30 08:06:29 -0400

    bootstrapped the backend

---
+++ server/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.api import healthcheck
+
+app = FastAPI()
+
+app.include_router(healthcheck.router, prefix="/api", tags=["healthcheck"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Quiz App API!"}
