1c4d6d5e93015c5db2a4a36a8bde969c4c0108b9
Glorycornel <104757918+Glorycornel@users.noreply.github.com>
2024-09-10 14:47:30 +0100

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
 from app.api import healthcheck
-from typing import Union
 
 app = FastAPI()
 
 app.include_router(healthcheck.router, prefix="/api", tags=["healthcheck"])
 
-@app.get("/")
+@app.get("/api")
 def read_root():
     return {"message": "Welcome to the Quiz App API!"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
