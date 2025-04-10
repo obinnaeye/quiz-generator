e73e421f311a18bba559a53b098f090e87693159
Fabrice Azonhin <azonhinfab@gmail.com>
2025-04-10 11:19:10 +0100

    Initial commit on password reset

--- server/email_utils.py
+++ server/email_utils.py
@@ -10,16 +10,29 @@ SMTP_PORT = 587
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")  
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 
-def send_otp_email(email: str, otp: str, token: str):
-    subject = "Please verify your account on Quiz Generator"
-    verification_link = f"http://localhost:8000/verify-link/?token={token}"
-    body = f"""
+def send_otp_email(email: str, otp: str, token: str, mode="register"):
+    if mode == "reset":
+        subject = "Reset your password on Quiz Generator"
+        verification_link = f"http://localhost:8000/reset-password-link/?token={token}"
+        body = f"""
+        You requested to reset your password.
 
-    Thank you for registering!
-    
-    To verify your email, you can either:
-    1. Enter the OTP: {otp}
-    2. Or click the following link: {verification_link}"""
+        You can either:
+        1. Enter this OTP: {otp}
+        2. Or click this link: {verification_link}
+
+        If you didn't request this, just ignore this message.
+        """
+    else:
+        subject = "Please verify your account on Quiz Generator"
+        verification_link = f"http://localhost:8000/verify-link/?token={token}"
+        body = f"""
+        Thank you for registering!
+
+        To verify your email, you can either:
+        1. Enter the OTP: {otp}
+        2. Or click the following link: {verification_link}
+        """
 
     msg = MIMEText(body)
     msg["Subject"] = subject
--- server/tasks.py
+++ server/tasks.py
@@ -2,10 +2,9 @@ from server.celery_config import celery
 from server.email_utils import send_otp_email
 
 @celery.task
-def send_otp_task(email: str, otp: str, token: str):
+def send_otp_task(email: str, otp: str, token: str, mode="register"):
     try:
-        return send_otp_email(email, otp, token)
+        return send_otp_email(email, otp, token, mode=mode)
     except Exception as e:
         print(f"Error sending OTP email: {e}")
         return False
-   
