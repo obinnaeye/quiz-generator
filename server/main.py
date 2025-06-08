a80aeb0796b8830209bf64c95e65ba553e16a931
Emmanuel Enwelunta <emmanuelenwelunta@gmail.com>
2025-06-08 14:15:37 +0100

    update CORS Middleware origins to wildcard for production staging

--- server/main.py
+++ server/main.py
@@ -36,7 +36,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
